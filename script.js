aea02f559b938b682e26ab1184b5522a9b800075
Jillian Amelung <jpamelung@gmail.com>
2021-05-16 19:54:12 -0400

    added trackHours function

--- script.js
+++ script.js
@@ -1,57 +1,32 @@
-$(document).ready(function (){ //tells browswer to load 1)html & 2) css first
+$(document).ready(function () { //tells browswer to load 1)html & 2) css first
 
 
- // display current date at top of caledar
- var todayDate = moment().format("dddd, MMM Do YYYY");
- $("#currentDay").html(todayDate);
- });
+  // display current date at top of caledar
+  var todayDate = moment().format("dddd, MMM Do YYYY");
+  $("#currentDay").html(todayDate);
+});
 
-$("#saveBtn").on("click", function () {
-  console.log(this);
 
-var text = $(this).siblings(".description").val();
-var time = $(this).parent().attr("id");
 
-localStorage.setItem(time, text);
+$(".saveBtn").on("click", function () {
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  //set items in local storage.
+  localStorage.setItem(time, text);
 })
 
+function trackHours () {
+  var currentTime = moment().hour();
 
+  $(".time-block").each(function () {
+    var hourBlock = pasrseInt($(this).attr("id).split"("hour")[1]);
+    console.log(hourBlock, currentTime)
 
+  if (hourBlock < currentTime) {
+    $(this).addClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+  }
 
-//  var currentTime = moment().hour();
-
-
-
-
-//  function timeTracker() {
-//   //get current number of hours.
-//   var timeNow = moment().hour();
-
-
-
-
-// //added curly bracket since it was missing
-
-
-
-
-//  // Click the save button, save to local storage
-
-//  // When I refresh the page, the save events persist
-
-// // ('#text').html("Text");
-// // localStorage.content = $('#text').html();
-// // $('#text').html(localStorage.content);
-
-// // var saveTasks = function() {
-// //     localStorage.setItem("#text", JSON.stringify(Text));
-// //   };
-
-// // var loadTasks = function() {
-// //     tasks = JSON.parse(localStorage.getItem("text"));
-// //   };
-
-
-// //   // display current date at top of caledar
-// //   var todayDate = moment().format("dddd, MMM Do YYYY");
-// //  	$("#currentDay").html(todayDate);
+  
