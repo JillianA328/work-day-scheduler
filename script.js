35de549e8031fb838ffcaee629682091e04f8636
Jillian Amelung <jpamelung@gmail.com>
2021-05-16 20:38:33 -0400

    added localStorage & fixed CSS to turn description boxes correct colors

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ $(document).ready(function () { //tells browswer to load 1)html & 2) css first
   $("#currentDay").html(todayDate);
 });
 
-
-
 $(".saveBtn").on("click", function () {
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
@@ -16,17 +14,44 @@ $(".saveBtn").on("click", function () {
   localStorage.setItem(time, text);
 })
 
-function trackHours () {
+function trackHours() {
   var currentTime = moment().hour();
 
   $(".time-block").each(function () {
-    var hourBlock = pasrseInt($(this).attr("id).split"("hour")[1]);
-    console.log(hourBlock, currentTime)
+    var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
 
-  if (hourBlock < currentTime) {
-    $(this).addClass("past");
-    $(this).removeClass("future");
-    $(this).removeClass("present");
-  }
+    if (hourBlock < currentTime) {
+      $(this).addClass("past");
+      $(this).removeClass("future");
+      $(this).removeClass("present");
 
-  
+    } else if (hourBlock === currentTime) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).removeClass("future");
+
+    } else {  
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+
+  })
+
+}
+
+trackHours();
+
+
+$("#hr9 .description").val(localStorage.getItem("hr9"));
+$("#hr10 .description").val(localStorage.getItem("hr10"));
+$("#hr11 .description").val(localStorage.getItem("hr11"));
+$("#hr12 .description").val(localStorage.getItem("hr12"));
+$("#hr13 .description").val(localStorage.getItem("hr13"));
+$("#hr14 .description").val(localStorage.getItem("hr14"));
+$("#hr15 .description").val(localStorage.getItem("hr15"));
+$("#hr16 .description").val(localStorage.getItem("hr16"));
+$("#hr17 .description").val(localStorage.getItem("hr17"));
+
+
+
